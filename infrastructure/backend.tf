c6d96244d4d6b8faf90dee5f71b0bf98dcb46382
samuelbartels20 <bartelssamuel20@gmail.com>
2025-08-22 17:03:42 +0000

    added architectural diagram

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,1 +1,28 @@
- 
+
+terraform {
+  backend "s3" {
+    bucket                      = "ionos-backend"
+    key                         = "wordpress-platform/terraform.tfstate"
+    region                      = "de"
+    endpoint                    = "https://S3-eu-central-1.ionoscloud.com"
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+    skip_region_validation      = true
+    force_path_style           = true
+  }
+  
+  required_providers {
+    ionos = {
+      source  = "ionos-cloud/ionos"
+      version = "~> 6.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.23"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.11"
+    }
+  }
+}
