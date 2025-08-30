32050662202250a699f0833bcdabac40fbb7e98d
samuelbartels20 <bartelssamuel20@gmail.com>
2025-08-30 15:53:57 +0000

    added files

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -1,29 +1,29 @@
 data "onepassword_item" "ionos_username" {
-  vault = "K8s"
+  vault = "xyrzokprou45dsa55meaoieqci"
   title = "ionos"
 }
 
 data "onepassword_item" "ionos_password" {
-  vault = "K8s"
+  vault = "xyrzokprou45dsa55meaoieqci"
   title = "ionos"
 }
 
 data "onepassword_item" "ionos_endpoint" {
-  vault = "K8s"
+  vault = "xyrzokprou45dsa55meaoieqci"
   title = "ionos"
 }
 
 data "onepassword_item" "ionos_token" {
-  vault = "K8s"
+  vault = "xyrzokprou45dsa55meaoieqci"
   title = "ionos"
 }
 
 
-data "onepassword_vault" "K8s" {
-  name = "K8S"
+data "onepassword_vault" "xyrzokprou45dsa55meaoieqci" {
+  name = "xyrzokprou45dsa55meaoieqci"
 }
 
 data "onepassword_item" "ionos" {
-  vault = data.onepassword_vault.K8s.id
+  vault = data.onepassword_vault.xyrzokprou45dsa55meaoieqci.id
   title = "ionos"
 }
