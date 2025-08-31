a929528e84e15f2b4500baa3f608c5295ed68941
samuelbartels20 <bartelssamuel20@gmail.com>
2025-08-31 17:12:00 +0000

    added files

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -3,6 +3,9 @@ terraform {
     bucket = "ionos202030"
     key    = "terraform.tfstate"
     region = "us-east-1"
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
+    skip_credentials_validation = true
   }
 }
 
@@ -16,4 +19,10 @@ provider "ionoscloud" {
 
 provider "onepassword" {
   service_account_token = var.op_service_account_token
+}
+
+provider "aws" {
+  region = "us-east-1"
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
--- infrastructure/variable.tf
+++ infrastructure/variable.tf
@@ -134,6 +134,16 @@ variable "item" {
   default     = "ionos"
 }
 
+variable "aws_access_key_id" {
+  description = "AWS access key ID"
+  type        = string
+  sensitive   = true
+}
+variable "aws_secret_access_key" {
+  description = "AWS secret access key"
+  type        = string
+  sensitive   = true
+}
 
 
 
