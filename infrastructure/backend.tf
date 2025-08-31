2b743cce184bf20474713c5005092ff0e7db9dba
samuelbartels20 <bartelssamuel20@gmail.com>
2025-08-31 18:19:43 +0000

    added files

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -23,6 +23,6 @@ provider "onepassword" {
 
 provider "aws" {
   region = "us-east-1"
-  # access_key = var.aws_access_key_id
-  # secret_key = var.aws_secret_access_key
+  access_key = var.aws_access_key_id
+  secret_key = var.aws_secret_access_key
 }
