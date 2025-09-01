435bfb42993c80a65b1ad7b06dc25767576bbab0
samuelbartels20 <bartelssamuel20@gmail.com>
2025-09-01 01:29:22 +0000

    added files

--- infrastructure/data.tf
+++ infrastructure/data.tf
@@ -2,8 +2,8 @@ data "onepassword_vault" "vault" {
   name = var.vault
 }
 
-data "onepassword_item" "item" {
-  vault = data.onepassword_vault.vault.name
-  title = var.item
-}
+# data "onepassword_item" "item" {
+#   vault = data.onepassword_vault.vault.name
+#   title = var.item
+# }
 
